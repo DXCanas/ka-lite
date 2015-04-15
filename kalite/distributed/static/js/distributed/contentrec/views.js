8d7e1c2ddf1ddd39abaadfc81f5865cf59690724
David <david@FLEDev.(none)>
2015-04-15 13:47:20 -0700

    Made template, laid out foundation in views.js, made blank handlebars files for each view

--- kalite/distributed/static/js/distributed/contentrec/views.js
+++ kalite/distributed/static/js/distributed/contentrec/views.js
@@ -3,6 +3,21 @@
 /**
  * View that wraps the content resume card on the learn page
  */
+
+window.ContentRecLanding = BaseView.extend({
+
+    template: HB.template("contentrec/content-rec-landing"),
+    
+    initialize: function() {
+        this.model = new Backbone.Model();
+        this.render();
+    },
+
+    render: function() {
+        this.$el.html(this.template(this.model.attributes));
+    }
+
+});
 window.ContentResumeView = BaseView.extend({
 
     template: HB.template("contentrec/content-resume"),
@@ -18,10 +33,43 @@ window.ContentResumeView = BaseView.extend({
 
 });
 
+window.ContentRecView = BaseView.extend({
+
+    template: HB.template("contentrec/content-rec"),
+    
+    initialize: function() {
+        this.model = new Backbone.Model();
+        this.render();
+    },
+
+    render: function() {
+        this.$el.html(this.template(this.model.attributes));
+    }
+
+});
+
 $(function(){
 	//window.content_resume = new ContentResumeView();
 	//$("#content-area").append(window.content_resume.$el);
-	window.content_resume = new ContentResumeView({
+	window.content_rec_landing = new ContentRecLanding({
 		el: "#content-area"
+	});
+});
+
+$(function ResumeCard(resumeContainer){
+	window.content_resume = new ContentResumeView({
+		el:"#resumeCard"
+	});
+});
+
+$(function NextStepsCard(nextStepsContainer){
+	window.next_steps = new NextStepsView({
+		el:"#nextStepsCard"
+	});
+});
+
+$(function ContentRecCard(contentRecContainer){
+	window.content_rec = new ContentRecView({
+		el:"#contentRecCard"
 	});
 });
